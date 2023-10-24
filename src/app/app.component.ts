eac9c5dd57c6f604f711b3a6d4060147472fcc6f
Darina <dajtbaeva@gmail.com>
2023-10-24 19:28:58 +0600

    upd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-// import { Observable } from 'rxjs';
-// import { IComment } from './models/comment';
 import { IPost } from './models/post';
 import { PostsService } from './services/posts.service';
 
@@ -13,16 +11,11 @@ export class AppComponent implements OnInit {
   title = 'posts-app';
   userId = '';
   posts: IPost[] = [];
-  // posts$: Observable<IPost[]>;
-  // stream: type<generic>
-
   constructor(private postsService: PostsService) {}
-
   ngOnInit(): void {
     throw new Error('Method not implemented.');
   }
   showPosts() {
-    // this.posts$ = this.postsService.getPosts(this.userId);
     this.postsService.getPosts(this.userId).subscribe((data) => {
       this.posts = data;
     });
